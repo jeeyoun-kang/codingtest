e3000c356a67bfad2e8b95e5ce421c1ed248b891
JYKANG <xadamantiumx@naver.com>
2022-09-03 00:48:15 +0900

    0903-1248

--- 이코테/1이 될때까지.py
+++ 이코테/1이 될때까지.py
@@ -1,5 +1,6 @@
+import time
 
-
+start_time = time.time()
 count = 0
 n, k = map(int, input().split())
 while (n !=1):
@@ -14,3 +15,21 @@ while (n !=1):
         n -= result
         count +=result
 print(count)
+# n, k = map(int, input().split())
+#
+# result = 0
+#
+# while True:
+#     target = (n // k) * k
+#     result += (n - target)
+#     n = target
+#
+#     if n < k:
+#         break
+#     result += 1
+#     n = n // k
+#
+# result += (n - 1)
+# print(result)
+end_time = time.time()
+print("time=",end_time - start_time)
