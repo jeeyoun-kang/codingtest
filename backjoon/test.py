a7deb0850799016837cfc192901d49c5b818c4ed
JYKANG <xadamantiumx@naver.com>
2022-12-02 23:06:41 +0900

    1202-2306

--- backjoon/test.py
+++ backjoon/test.py
@@ -1,8 +1,20 @@
-N = int(input())
-array = []
-for i in range(N):
-    array.append(int(input()))
-    array.sort()
-for i in array:
-    print(i)
-
+cro = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+n = input()
+count=0
+i=0
+while(i!=len(n)):
+    if n[i:i+2] in cro:
+        if "=" in n[i:i+2]:
+            count+=1
+        else:
+            count+=2
+        i+=2
+    elif n[i:i+3] in cro:
+        if "=" in n[i:i+3]:
+            count+=2
+        else:
+            count+=3
+        i+=3
+    else:
+        i+=1
+print(count)
