5f1aeacaf62c59b753b772debc5c9643251f15b0
JYKANG <xadamantiumx@naver.com>
2022-12-28 00:40:56 +0900

    1228-1240

--- backjoon/test.py
+++ backjoon/test.py
@@ -1,7 +1,10 @@
-from sys import stdin
-N = int(stdin.readline().rstrip())
-A = list(map(int,stdin.readline().split()))
-M = int(stdin.readline())
-X = list(map(int,stdin.readline().split()))
-for i in X:
-    print(1) if i in A else print(0)
+t = int(input())
+
+for _ in range(t):  
+    floor = int(input())  # 층
+    num = int(input())  # 호
+    f0 = [x for x in range(1, num+1)]  # 0층 리스트
+    for k in range(floor):  # 층 수 만큼 반복
+        for i in range(1, num):  # 1 ~ n-1까지 (인덱스로 사용)
+            f0[i] += f0[i-1]  # 층별 각 호실의 사람 수를 변경
+    print(f0[-1])  # 가장 마지막 수 출력
