0781dc82455d3eac63bbdac8ab6c9a8b7ba5f469
JYKANG <xadamantiumx@naver.com>
2023-01-19 11:42:55 +0900

    01191142

--- algorithm-study/final.py
+++ algorithm-study/final.py
@@ -1,21 +1,21 @@
 import time
+import time
 start_time = time.time() # 측정 시작
+
 import sys
 input = sys.stdin.readline
-villiage = []
-all_people = 0
-N = int(input())
-for i in range(N):
-    n_viliage, people = map(int, input().split())
-    villiage.append([n_viliage, people])
-    all_people += people
 
-villiage.sort(key= lambda x: x[0])
-count = 0
-for i in range(N):
-    count += villiage[i] [1]
-    if count >= all_people/2:
-        print (villiage[i][0])
-        break
+n, k = map(int, input().split())
+numbers = input().rstrip()
+stack = []
+for number in numbers:
+    while stack and stack[-1] < number and k > 0:
+        stack.pop()
+        k -= 1
+    stack.append(number)
+if k > 0:
+    print(''.join(stack[:-k]))
+else:
+    print(''.join(stack))
 end_time = time.time() # 측정 종료
 print("time:", end_time - start_time) # 수행 시간 출력
--- algorithm-study/test.py
+++ algorithm-study/test.py
@@ -1,27 +1,35 @@
+# K개를 지웠을 때 얻을 수 있는 가장 큰 수를 출력
+import time
+start_time = time.time() # 측정 시작
+
 
 import sys
 input = sys.stdin.readline
+n,k= map(int,input().split())
+number =list(input().strip())
+count=0
+result=0
+#맨앞부분을 기준으로 정리
+for i in range(2,n+2):
+    count+=1
+    if number[0]<number[i-1]:
+        result=max(result,int(number[i-1]))
+    if result>=int(number[i]): #max값이 바로 다음 값보다 클때
+        break
 
-n = int(input())
+for j in range(count):
+    number.pop(0)
+findmin=list(number)
+findmin.sort() #최솟값 두개를 찾기위한 리스트 정렬
 
-loc = [0 for _ in range(n+1)]
-per = [0 for _ in range(n+1)]
-dis = 0  # 사람*위치 합친 거리
-count=0 # 사람 수만 합친 수
-test=0
-result=[0 for _ in range(n+1)]
-for i in range(n):
-    x,a = map(int,input().split())
-    loc[x]=x
-    per[x]=a
-
-for j in range(1,n+1): #각 마을별 비용 구하기
-    for k in range(1,n+1):
-        test+=abs(k-j)*per[k]
-    result[j]=test
-    test=0
-result.pop(0)
-print(result.index(min(result))+1)
-
-
-
+for i in range(k-count):
+    if number.index(findmin[0])<number.index(findmin[1]):
+        number.pop(number.index(findmin[0]))
+        findmin.pop(0)
+    else:
+        number.pop(number.index(findmin[1]))
+        findmin.pop(1)
+print("".join(number))
+# 프로그램 소스코드
+end_time = time.time() # 측정 종료
+print("time:", end_time - start_time) # 수행 시간 출력
