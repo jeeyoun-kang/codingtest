3cdaeb2ca38bebbffb3075de9bcb273a1da77681
JYKANG <xadamantiumx@naver.com>
2023-01-20 11:51:25 +0900

    0120-1151

--- algorithm-study/test.py
+++ algorithm-study/test.py
@@ -1,35 +1,34 @@
-# K개를 지웠을 때 얻을 수 있는 가장 큰 수를 출력
-import time
-start_time = time.time() # 측정 시작
-
+#기프티콘 n개중 기한 가장 적게 남은 기프티콘만 사용가능
+#기프티콘 기한 연장을 최소한으로 하고 싶어함
+#최소횟수로 기한 연장하면서 기프티콘 다쓰게(기한맞춰서 기프티콘쓰게해야함)
 
 import sys
-input = sys.stdin.readline
-n,k= map(int,input().split())
-number =list(input().strip())
-count=0
-result=0
-#맨앞부분을 기준으로 정리
-for i in range(2,n+2):
-    count+=1
-    if number[0]<number[i-1]:
-        result=max(result,int(number[i-1]))
-    if result>=int(number[i]): #max값이 바로 다음 값보다 클때
-        break
+import math
+input=sys.stdin.readline
+n=int(input())
+deadline=list(map(int,input().split()))
+plan=list(map(int,input().split()))
+test=0
+count = 0
+for k in range(n): #연장일보다 기한일 늘리게 함
 
-for j in range(count):
-    number.pop(0)
-findmin=list(number)
-findmin.sort() #최솟값 두개를 찾기위한 리스트 정렬
+    if deadline[k]<plan[k]: #기한연장해야되는 기프티콘이 있는경우
+        test=(int(plan[k])-int(deadline[k]))//30
+        deadline[k]=30*(test + 1)+deadline[k]
+        count += test + 1
 
-for i in range(k-count):
-    if number.index(findmin[0])<number.index(findmin[1]):
-        number.pop(number.index(findmin[0]))
-        findmin.pop(0)
-    else:
-        number.pop(number.index(findmin[1]))
-        findmin.pop(1)
-print("".join(number))
-# 프로그램 소스코드
-end_time = time.time() # 측정 종료
-print("time:", end_time - start_time) # 수행 시간 출력
+print(deadline)
+
+#사용할 날짜 비교하면서 더 연장할 기프티콘은 연장하기(수정하기)-스택이용해보기
+for j in range(n):
+    minplan = plan.index(min(plan))
+    mindead = deadline.index(min(deadline))
+    if mindead!=minplan:
+        deadline[mindead] =30+deadline[mindead]
+        count+=1
+        print(deadline)
+print(count)
+
+if min(plan)==sum(plan)/n:
+    count-=math.ceil(count/2)
+print(count)
--- algorithm-study/test3.py
+++ algorithm-study/test3.py
@@ -1,20 +1,3 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-ch2 = [0]*(N+1)
-
-for i in range(N):
-    a,b,c = map(int,input().split())
-    ch2[a] = c
-
-t = 1
-cnt = -1
-while True:
-    t = ch2[t]
-    cnt += 1
-    if t == -1:
-        break
-
-
-print((N-1)*2 - cnt)
+a=1
+b=2
+print(round(a/b))
