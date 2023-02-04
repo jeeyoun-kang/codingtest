80804183dd69f2884489083ecb42c85d86bf49ad
JYKANG <xadamantiumx@naver.com>
2023-02-04 22:15:03 +0900

    0204-2214

---
+++ backjoon/test.py
@@ -0,0 +1,54 @@
+# import sys
+# input = sys.stdin.readline
+#
+# n,m = map(int, input().split())
+# graph=[]
+# for i in range(n):
+#     a=list(map(int,input().split()))
+#     graph.append(a)
+# print(graph)
+# for j in range(n):
+#     for k in range(m):
+#         graph[m][n]
+from collections import deque
+
+m, n = map(int, input().split())
+graph = []
+queue = deque([])
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+    for j in range(m):  # 익은 토마토 큐에 저장
+        if graph[i][j] == 1:
+            queue.append([i, j])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+count=0
+def bfs():
+    global count
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            a = x + dx[i]
+            b = y + dy[i]
+            if 0 <= a < n and 0 <= b < m :
+                count=max(graph[a][b],count)
+        +count
+                # queue.append([a, b])
+                # graph[a][b] = graph[x][y] + 1
+                # print(graph)
+
+
+bfs()
+# answ = 0
+# for i in graph:
+#     for j in i:
+#         if j == 0:
+#             print(-1)
+#             exit(0)
+#     answ = max(answ, max(i))
+print(graph)
+# print(answ - 1)
