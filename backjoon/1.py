f41a497a0b6449ef48e67eb1d976b84045af3b1d
JYKANG <xadamantiumx@naver.com>
2023-02-26 23:38:51 +0900

    0226-2338

--- backjoon/1.py
+++ backjoon/1.py
@@ -1,27 +1,24 @@
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-stone = []
-
-# dp 배열 생성
-dp = [1e9]*N
-dp[0] = 0
-for i in range(N-1):
-    s, b = map(int, input().split())
-    stone.append((s, b))
-    if i+1<N : dp[i+1] = min(dp[i+1], dp[i]+s)
-    if i+2<N : dp[i+2] = min(dp[i+2], dp[i]+b)
-
-# 매우 큰 점프 적용해보며 최솟값 찾기
-K = int(input())
-_min = dp[-1]
-for i in range(3, N):
-    e, dp1, dp2 = dp[i-3]+K, 1e9, 1e9
-    for j in range(i, N-1):
-        if i+1<=N : dp1 = min(dp1, e+stone[j][0])
-        if i+2<=N : dp2 = min(dp2, e+stone[j][1])
-        e, dp1, dp2 = dp1, dp2, 1e9
-    _min = min(_min, e)
-
-print(_min)
+n, m = map(int, sys.stdin.readline().split())
+dot = sorted(list(map(int, sys.stdin.readline().split())))
+for _ in range(m):
+    left = 0
+    right = n - 1
+    x, y = map(int, sys.stdin.readline().split())
+    while left <= right:
+        mid=(left+right)//2
+        if dot[mid] < x:
+            left =mid+1
+        else:
+            right=mid-1
+    firstIdx=left
+    left,right=0,n-1
+    while left<=right:
+        mid=(left+right)//2
+        if dot[mid]>y:
+            right=mid-1
+        else:
+            left=mid+1
+    endIdx=right+1
+    print(endIdx-firstIdx)
