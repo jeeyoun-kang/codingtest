65c4e4d19018cfc19577c4aa91ffe5ce0381031c
JYKANG <xadamantiumx@naver.com>
2023-04-14 20:39:30 +0900

    0414-2039

--- 이코테/1.py
+++ 이코테/1.py
@@ -1,6 +1,24 @@
-for x in range(4):
-  if x == 2:
-    print ('loop out')
-    break
-else:
-  print ('loop end')
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+P = []
+num_list = []
+for i in range(t):
+    P.append(input().strip())
+    n = int(input())
+    num_list.append(list(input()))
+print(num_list)
+# for j in range(t):
+#     test = "".join(P[j])
+#     print(test)
+#     test=test.pop()
+#     if test == 'R':
+#         test2 = num_list[j]
+#         test2 = test2[::-1]
+#         num_list[j] = test2
+#     else:
+#         test3 = num_list[j]
+#         test3 = test3.pop()
+#         num_list[j] = test3
