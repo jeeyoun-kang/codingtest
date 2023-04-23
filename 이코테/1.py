e7bb6e3ca814e737a2434ed245800b97289bd3a8
JYKANG <xadamantiumx@naver.com>
2023-04-23 23:27:53 +0900

    0423-2327

--- 이코테/1.py
+++ 이코테/1.py
@@ -10,15 +10,3 @@ for i in range(t):
     n = int(input())
     num_list.append(list(input()))
 print(num_list)
-# for j in range(t):
-#     test = "".join(P[j])
-#     print(test)
-#     test=test.pop()
-#     if test == 'R':
-#         test2 = num_list[j]
-#         test2 = test2[::-1]
-#         num_list[j] = test2
-#     else:
-#         test3 = num_list[j]
-#         test3 = test3.pop()
-#         num_list[j] = test3
