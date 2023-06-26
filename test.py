6957b524a9c0a133cf5df4c94bb808a2f4ce5b99
JYKANG <xadamantiumx@naver.com>
2023-06-26 22:38:22 +0900

    0626

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
-#이코테-금광
-t=int(input())
-for i in range(t):
-    n,m=map(int,input())
-    gold=list(map(int,input().split()))
-    test=gold[0]
+from collections import Counter
 
+
+def solution(S):
+    count = Counter(S)
+    max_removal = float('inf')
+
+    # "B", "A", "N"의 개수 중 가장 작은 값 구하기
+    for char in "BAN":
+        max_removal = min(max_removal, count[char])
+
+    return max_removal
+
+
+# 테스트
+print(solution("NAABXXAN"))  # 1
+print(solution("NAANAAXNABABYNNBZ"))  # 2
+print(solution("QABAAAWOBL"))  # 0
---
+++ test3.py
@@ -0,0 +1,17 @@
+import sys
+n = int(input())
+answer=[]
+for i in range(n):
+    paper = list(map(int,input().split()))
+    answer.append(map)
+blue=0
+white=0
+
+
+def cut(x,y,max): # 현재위치,체크해야할 범위
+    
+
+    return 0
+
+
+cut(0,0,n)
