c06f5e7b35727cfe4e64f8be82e35988264e556e
JYKANG <xadamantiumx@naver.com>
2023-09-01 22:49:13 +0900

    0901

---
+++ backjoon/DFSBFS/5639.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+nums = []
+while True:
+    try:
+        nums.append(int(input()))
+    except:
+        break
+
+def postorder(s, e):
+    if s > e:
+        return
+    mid = e + 1
+
+    for i in range(s + 1, e + 1):
+        if nums[s] < nums[i]:
+            mid = i
+            break
+
+    postorder(s + 1, mid - 1)
+    postorder(mid, e)
+    print(nums[s])
+
+
+postorder(0, len(nums) - 1)
+
